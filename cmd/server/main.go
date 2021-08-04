254f4910d4b17cb6c71b486dc9de9453e6db1846
mikouhero <33013997+mikouhero@users.noreply.github.com>
2021-08-04 17:24:35 +0800

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,28 @@
+package main 
 
+
+// 自定义定义需要的参数
+
+
+// 定义命令行flag参数，便于解析
+
+
+func init() {
+  
+  // todo
+  //flag.XXX
+  
+  flag.Parse()
+}
+
+
+func main() {
+    
+  //解析数据 返回 request 对象
+  
+  
+  // 分发数据 开始处理
+  
+  
+
+}
